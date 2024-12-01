a03b4468754509273c46a1fd72083e5363c13cdc
Vitaliy Derda <derdavitaliy@gmail.com>
2024-12-01 09:12:15 +0200

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace LearnBlazor.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool IsActive { get; set; }
+    public required IEnumerable<ProductProp> ProductProperties { get; set; }
+}
---
+++ Models/ProductProp.cs
@@ -0,0 +1,8 @@
+﻿namespace LearnBlazor.Models;
+
+public class ProductProp
+{
+    public int Id { get; set; }
+    public required string Key { get; set; }
+    public required string Value { get; set; }
+}
