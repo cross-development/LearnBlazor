01bc6ca2f234f19835a7ef0816778b6b404cd247
Vitaliy Derda <derdavitaliy@gmail.com>
2024-12-09 18:43:23 +0200

    feat: added js runtime examples

---
+++ Helpers/JsRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helpers;
+
+public static class JsRuntimeExtensions
+{
+    public static async Task ToasterSuccess(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showToaster", "success", message);
+    }
+
+    public static async Task ToasterError(this IJSRuntime jsRuntime, string message)
+    {
+        await jsRuntime.InvokeVoidAsync("showToaster", "error", message);
+    }
+}
---
+++ wwwroot/js/common.js
@@ -0,0 +1,13 @@
+﻿window.showToaster = function (type, message) {
+    if (type === "success") {
+        toastr?.success(message);
+    }
+
+    if (type === "error") {
+        toastr?.error(message);
+    }
+}
+
+window.showSwal = function (type, title, message) {
+    Swal.fire({ title, text: message, icon: type });
+}
